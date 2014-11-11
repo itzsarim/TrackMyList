09f5e0a13031cbdad11ced68655d0f4181ead6e0
Henry <lixpbd950@gmail.com>
2014-11-11 16:42:29 -0500

    Revert "updated index.js and oauth.js"
    This reverts commit 551ed50fca578beb8df2a6fae3e62fd8764adf96.

--- www/js/database.js
+++ www/js/database.js
@@ -125,7 +125,6 @@ function updateTable(tableName,setFields,setParams,whereStr,wherParams){
 		sql += " where " + whereStr;
 		setParams = setParams.concat(wherParams);
 	}
-	//alert(sql+" "+setParams[0]+" "+setParams[1]);
 	execSql(sql,setParams);
 }
 
@@ -180,7 +179,7 @@ function select(tableName,selectFields,whereStr,wherParams,callback){
 		&& whereStr!=""){
 		sql += " where " + whereStr;
 	}
-   	//alert(sql);
+   // alert(sql);
 	 db.transaction(function(tx){
           tx.executeSql(sql,wherParams,function(tx,results){
         	  if(results.rows.length<1){
@@ -189,9 +188,9 @@ function select(tableName,selectFields,whereStr,wherParams,callback){
               	  if(typeof(callback) == 'function') {callback(results.rows)}
         	  }
           },function(tx,error){
-          	alert("error");
               return false;
           });
       });
 }
  
+
